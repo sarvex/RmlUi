3140f0949afa25b864c4e0772ed35e0a5783fa97
Michael Ragazzon <michael.ragazzon@gmail.com>
2022-02-17 23:40:33 +0100

    Experimental, update render effect API. Box shadow is now always rendered at native size, and its 2d texture is projected with transform. This way we don't need to re-render it when the element's transform changes, but bitmap scaling may not look as good in all transform situations.

--- Source/Core/DecoratorGradient.cpp
+++ Source/Core/DecoratorGradient.cpp
@@ -299,7 +299,10 @@ void DecoratorLinearGradient::RenderElement(Element* element, DecoratorDataHandl
 	if (!render_interface)
 		return;
 
-	render_interface->RenderEffect(CompiledEffectHandle(element_data), render_stage, 0, element);
+	if (render_stage == RenderStage::Decoration)
+	{
+		render_interface->RenderEffect(CompiledEffectHandle(element_data), RenderSource::Stack, RenderTarget::Stack);
+	}
 }
 
 DecoratorLinearGradientInstancer::DecoratorLinearGradientInstancer()
