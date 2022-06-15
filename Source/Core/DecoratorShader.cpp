e6d682c1c7bb64b504d4ea53c1ec0b3612f6012f
Michael Ragazzon <michael.ragazzon@gmail.com>
2022-06-16 00:27:28 +0200

    Fix render effects rounding

--- Source/Core/DecoratorShader.cpp
+++ Source/Core/DecoratorShader.cpp
@@ -102,7 +102,7 @@ void DecoratorShader::ReleaseElementData(DecoratorDataHandle handle) const
 void DecoratorShader::RenderElement(Element* element, DecoratorDataHandle handle) const
 {
 	BasicEffectElementData* element_data = reinterpret_cast<BasicEffectElementData*>(handle);
-	element_data->render_interface->RenderEffect(element_data->effect, element_data->geometry, element->GetAbsoluteOffset(Box::BORDER));
+	element_data->render_interface->RenderEffect(element_data->effect, element_data->geometry, element->GetAbsoluteOffset(Box::BORDER).Round());
 }
 
 DecoratorShaderInstancer::DecoratorShaderInstancer() : DecoratorInstancer(DecoratorClasses::Background), ids{}
