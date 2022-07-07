fea94b9849f3119cef2c7f434bc533153845bf2a
Michael Ragazzon <michael.ragazzon@gmail.com>
2022-07-08 01:42:41 +0200

    Update effect sample with an option menu for applying filters and transforms

--- Samples/basic/effect/src/main.cpp
+++ Samples/basic/effect/src/main.cpp
@@ -74,6 +74,64 @@ int main(int /*argc*/, char** /*argv*/)
 	Shell::SetContext(context);
 	Shell::LoadFonts();
 
+	static constexpr float perspective_max = 3000.f;
+
+	struct EffectData {
+		bool show_menu = false;
+		Rml::String submenu = "filter";
+
+		struct Transform {
+			float scale = 1.0f;
+			Rml::Vector3f rotate;
+			float perspective = perspective_max;
+			Rml::Vector2f perspective_origin = Rml::Vector2f(50.f);
+			bool transform_all = false;
+		} transform;
+
+		struct Filter {
+			float opacity = 1.0f;
+			float sepia = 0.0f;
+			float grayscale = 0.0f;
+			float brightness = 1.0f;
+			float contrast = 1.0f;
+			float invert = 0.0f;
+			float blur = 0.0f;
+			bool drop_shadow = false;
+		} filter;
+	} data;
+
+	if (Rml::DataModelConstructor constructor = context->CreateDataModel("effects"))
+	{
+		constructor.Bind("show_menu", &data.show_menu);
+		constructor.Bind("submenu", &data.submenu);
+
+		constructor.Bind("scale", &data.transform.scale);
+		constructor.Bind("rotate_x", &data.transform.rotate.x);
+		constructor.Bind("rotate_y", &data.transform.rotate.y);
+		constructor.Bind("rotate_z", &data.transform.rotate.z);
+		constructor.Bind("perspective", &data.transform.perspective);
+		constructor.Bind("perspective_origin_x", &data.transform.perspective_origin.x);
+		constructor.Bind("perspective_origin_y", &data.transform.perspective_origin.y);
+		constructor.Bind("transform_all", &data.transform.transform_all);
+
+		constructor.Bind("opacity", &data.filter.opacity);
+		constructor.Bind("sepia", &data.filter.sepia);
+		constructor.Bind("grayscale", &data.filter.grayscale);
+		constructor.Bind("brightness", &data.filter.brightness);
+		constructor.Bind("contrast", &data.filter.contrast);
+		constructor.Bind("invert", &data.filter.invert);
+		constructor.Bind("blur", &data.filter.blur);
+		constructor.Bind("drop_shadow", &data.filter.drop_shadow);
+
+		constructor.BindEventCallback("reset", [&data](Rml::DataModelHandle handle, Rml::Event& /*ev*/, const Rml::VariantList& /*arguments*/) {
+			if (data.submenu == "transform")
+				data.transform = EffectData::Transform{};
+			else if (data.submenu == "filter")
+				data.filter = EffectData::Filter{};
+			handle.DirtyAllVariables();
+		});
+	}
+
 	// Load and show the tutorial document.
 	if (Rml::ElementDocument* document = context->LoadDocument("basic/effect/data/effect.rml"))
 		document->Show();
