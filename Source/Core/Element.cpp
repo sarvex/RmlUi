c735cee057c3681fae244e1c1037fa19681347e9
Michael Ragazzon <michael.ragazzon@gmail.com>
2023-06-03 00:07:13 +0200

    Add clip mask to render interface, introduce render manager to keep track of the render state
    - The clip mask can be rendered to using normal geometry, then during other render commands the clip mask should hide any contents outside its area.
    - Improved element clipping behavior: Handles more complicated cases, including nested transforms with hidden overflow, and clips to the curved edge of elements with border-radius.
    - Text culling now also considers the viewport and properly handles transforms. Previously, text was not rendered in some situations, or unnecessarily rendered outside the window.
    - Clip mask implemented in GL3 renderer using stencil buffer.
    - Added and modified visual tests for clipping behavior.

--- Source/Core/Element.cpp
+++ Source/Core/Element.cpp
@@ -1529,6 +1529,11 @@ String Element::GetEventDispatcherSummary() const
 	return meta->event_dispatcher.ToString();
 }
 
+ElementBackgroundBorder* Element::GetElementBackgroundBorder() const
+{
+	return &meta->background_border;
+}
+
 ElementDecoration* Element::GetElementDecoration() const
 {
 	return &meta->decoration;
